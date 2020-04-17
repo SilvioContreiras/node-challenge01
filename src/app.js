c115766bb2899a651652c4c7a89d8ce1e657d9b2
SilvioContreiras <silvio7dcontreiras@gmail.com>
2020-04-17 01:50:09 -0300

    feat(routes): added routes to create, update and delete

--- src/app.js
+++ src/app.js
@@ -4,26 +4,100 @@ const { uuid } =  require('uuidv4')
 
 const app = express();
 
-
 app.use(express.json());
 
 app.use(cors());
 
 const repositories = [];
+const techs =  [];
 
-app.get('/', (request, response) => {
+app.get('/repositories', (request, response) => {
+  response.json({ message: 'Ola mundo'})
+});
+
+app.post('/repositories', (request, response) => {
+  const { title, url } =  request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs: [],
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  response.json(repository)
 
 });
 
-app.post('/', (request, response) => {
+
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, url } = request.body;
+
+  const repoIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exists' })
+  }
+
+  const repository = {
+    title,
+    url,
+    techs: techs,
+  }
+});
+
+
+app.delete('/repositories/id', (request, response) => {
+  const { id } = resquest.param;
+
+  const repositoryIndex =  repositories.findIndex(repository => repository.id === id );
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository Id does not exists!' });
+  }
+
+   repositories.splice(repositoryIndex, 1);
+
+   response.status(204).send()
 
 });
 
-app.put('/', (request, response) => {
 
+app.post('/repositories/:id/techs', (request, response) => {
+  const { id }  = request.params;
+  const { tech } = request.body;
+
+  const repositoryIndex =  repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository does not exists!" });
+  }
+
+  const arrTechs = techs.push(tech);
+
+  repositories.techs.push(arrTechs);
+
+  response.status(204).send();
+  
 });
 
-app.delete('/', (request, response) => {
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params;
+  const { like } = request.param;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exists!' });
+  }
+  
+  repositories.likes = 1 + linke
+
+  response.status(204).send();
 
 });
 
